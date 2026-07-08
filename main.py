7cc6bbbe7028526c3623f582a1535f9ced31bb7b
Praneet Raj Lingamallu <praneet.raj.lingamallu@googlemail.com>
2026-07-08 17:40:19 +0530

    port corrected

--- main.py
+++ main.py
@@ -22,3 +22,15 @@ app = FastAPI(
 )
 
 app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", "8003"))
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=True,
+    )
